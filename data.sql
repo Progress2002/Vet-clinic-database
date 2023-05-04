4cce6f8ddc4138d9a868f7a928008a34dd956b51
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-05-05 00:11:36 +0100

    Update data.sql, queries.sql and schema.sql

--- data.sql
+++ data.sql
@@ -1,20 +1,44 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals VALUES (1, 'Agumon', '2020-2-3', 0, true, 10.23);
-INSERT INTO animals VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
-INSERT INTO animals VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
-INSERT INTO animals VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
+INSERT INTO animals (name) VALUES ('Luna');
+INSERT INTO animals (name) VALUES ('Daisy');
+INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
+VALUES ('Agumon', '2020-02-03', 10.23, true, 0),
+( 'Gabumon', '2018-11-15', 8.00, TRUE, 2),
+('Pikachu', '2021-01-07', 15.04, FALSE, 1),
+('Devimon', '2017-05-12', 11.00, TRUE, 5);
+('Charmander', '2020-02-08', -11.00, FALSE, 0),
+('Plantmon', '2021-11-15', -5.70, TRUE, 2),
+('Squirtle', '1993-04-02', -12.13, FALSE, 3),
+('Angemon', '2005-06-12', -45.00, TRUE, 1),
+('Boarmon', '2005-06-07', 20.40, TRUE, 7),
+('Blossom', '1998-10-13', 17.00, TRUE,3),
+('Ditto', '2022-05-14', 22.00, TRUE, 4);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander', '2020-02-8', 0, false, -11);
+INSERT INTO owners (full_name, age)
+ VALUES ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (6, 'Plantmon', '2021-11-15', 2, true, -5.7);
+INSERT INTO species (name)
+ VALUES ('Pokemon'),
+  ('Digimon');
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (7, 'Squirtle', '1993-04-2', 3, false, -12.13);
+UPDATE animals     
+ SET species_id = CASE
+   WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+   ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+END;
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (8, 'Angemon', '2005-06-12', 1, true, -45);
-
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
-
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', '1998-10-13', 3, true, 17);
-
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (11, 'Ditto', '2022-05-14', 4, true, 22);
+UPDATE animals
+SET owner_id = CASE
+    WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+    WHEN name = 'Gabumon' OR name = 'Pikachu' THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+    WHEN name = 'Devimon' OR name = 'Plantmon' THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+    WHEN name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom' THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+    WHEN name = 'Angemon' OR name = 'Boarmon' THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+END;

bca8835b63950468d70899c454cd0de546574702
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-05-08 19:35:22 +0000

    Check queries performance

--- queries.sql
+++ queries.sql
@@ -177,3 +177,8 @@ AND animals.species_id = species.id
 GROUP BY vet_name, species_name
 ORDER BY number_visits DESC
 LIMIT 1;  
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+

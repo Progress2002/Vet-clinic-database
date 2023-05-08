6fb25f8f808cabf0224cbf3246d3a68a7258057a
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-05-08 20:39:28 +0100

    Create index to decrease execution time

--- queries.sql
+++ queries.sql
@@ -182,3 +182,6 @@ EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
+CREATE INDEX visits_idex ON visits (animal_id ASC);
+CREATE INDEX visits_idex ON visits (vet_id ASC);
+CREATE INDEX owners_idex ON owners (email ASC);
